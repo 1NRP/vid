9d0c62928cc687419d6b8fdb01c0423c04a30312
Ashwin Kumar <135273362+1NRP@users.noreply.github.com>
2024-09-01 03:04:31 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -27,19 +27,21 @@ self.addEventListener("fetch", function (event) {
     })
   );
 });
-
 */
+
 var staticCacheName = "NRP-PWA";
  
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll(["/vid"]);
     })
   );
 });
  
 self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+ 
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
