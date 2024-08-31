5428e68744641fe1604d0837a1a2a38b9d98fad2
Ashwin Kumar <135273362+1NRP@users.noreply.github.com>
2024-09-01 02:25:39 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,38 +1,28 @@
-var staticCacheName = "NRP-PWA";
-
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
-    })
+    // Define task to perform during installation if needed.
   );
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request).then(function (networkResponse) {
-        // Clone the response to modify headers
-        let modifiedResponse = networkResponse.clone();
+    fetch(event.request).then(function (networkResponse) {
+      // Clone the response to modify headers
+      let modifiedResponse = networkResponse.clone();
 
-        // Modify the headers of the response
-        const newHeaders = new Headers(modifiedResponse.headers);
-        newHeaders.set('Access-Control-Allow-Origin', '*');
+      // Modify the headers of the response
+      const newHeaders = new Headers(modifiedResponse.headers);
+      newHeaders.set('Access-Control-Allow-Origin', '*');
 
-        // Return a new response with the modified headers
-        return new Response(modifiedResponse.body, {
-          status: modifiedResponse.status,
-          statusText: modifiedResponse.statusText,
-          headers: newHeaders
-        });
+      // Return a new response with the modified headers
+      return new Response(modifiedResponse.body, {
+        status: modifiedResponse.status,
+        statusText: modifiedResponse.statusText,
+        headers: newHeaders
       });
     })
   );
 });
-
-
 
 /*
 var staticCacheName = "NRP-PWA";
