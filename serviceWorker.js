68d4f9212adaa7ea5258e4ad9903e271d257e1c1
Ashwin Kumar <135273362+1NRP@users.noreply.github.com>
2024-08-31 17:58:58 +0530

    ok

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,41 @@
 var staticCacheName = "NRP-PWA";
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(["/"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request).then(function (networkResponse) {
+        // Clone the response to modify headers
+        let modifiedResponse = networkResponse.clone();
+
+        // Modify the headers of the response
+        const newHeaders = new Headers(modifiedResponse.headers);
+        newHeaders.set('Access-Control-Allow-Origin', '*');
+
+        // Return a new response with the modified headers
+        return new Response(modifiedResponse.body, {
+          status: modifiedResponse.status,
+          statusText: modifiedResponse.statusText,
+          headers: newHeaders
+        });
+      });
+    })
+  );
+});
+
+
+
+/*
+var staticCacheName = "NRP-PWA";
  
 self.addEventListener("install", function (e) {
   e.waitUntil(
@@ -17,3 +54,4 @@ self.addEventListener("fetch", function (event) {
     })
   );
 });
+*/
