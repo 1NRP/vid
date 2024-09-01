776b8691e1e22d06398225726d84d85225b29241
Ashwin Kumar <135273362+1NRP@users.noreply.github.com>
2024-09-01 20:02:29 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,10 @@
-// Import required libraries
-importScripts("lib/config.js");
-importScripts("lib/chrome.js");
-importScripts("lib/runtime.js");
-importScripts("lib/webrequest.js");
-importScripts("lib/netrequest.js");
-importScripts("lib/common.js");
-
-var staticCacheName = "pwa";
+var staticCacheName = "NRP-PWA";
 
 // Install event - cache the application shell
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]); // Cache the root of the site
+      return cache.addAll(["/vid"]); // Cache the root of the site.
     })
   );
 });
@@ -20,7 +12,6 @@ self.addEventListener("install", function (e) {
 // Fetch event - serve cached content if available
 self.addEventListener("fetch", function (event) {
   console.log(event.request.url);
-
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request); // Serve from cache or fetch from network
